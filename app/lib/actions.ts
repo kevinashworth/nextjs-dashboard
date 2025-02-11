7aee2a18b9461613220ed00494463ee9c6936a6d
Kevin Ashworth <kevinashworth@yahoo.com>
2025-02-11 13:33:55 -0800

    Chapter 13

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -27,10 +27,16 @@ export async function createInvoice(formData: FormData) {
   const date = new Date().toISOString().split("T")[0];
   console.log({ customerId, amount, amountInCents, date, status });
 
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (e) {
+    console.error("createInvoice error:");
+    console.log(e);
+    return;
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
@@ -47,11 +53,17 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+  } catch (e) {
+    console.error("updateInvoice error:");
+    console.log(e);
+    return;
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
@@ -62,11 +74,16 @@ const DeleteInvoice = FormSchema.pick({ id: true });
 export async function deleteInvoice(id: string) {
   DeleteInvoice.parse({ id });
 
-  await sql`
-    DELETE FROM invoices
-    WHERE id = ${id}
-  `;
+  try {
+    await sql`
+      DELETE FROM invoices
+      WHERE id = ${id}
+    `;
+  } catch (e) {
+    console.error("deleteInvoice error:");
+    console.log(e);
+    return;
+  }
 
   revalidatePath("/dashboard/invoices");
-  // redirect("/dashboard/invoices");
 }
