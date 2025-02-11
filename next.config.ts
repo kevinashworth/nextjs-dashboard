f0f9a56a94fa9fdf7e84b9c7bbb038010133a2c6
Kevin Ashworth <kevinashworth@yahoo.com>
2025-02-10 23:36:33 -0800

    Finish fixing bcrypt seed mess

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  webpack: (config) => {
-    config.externals = [...config.externals, "bcrypt"];
-    return config;
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
