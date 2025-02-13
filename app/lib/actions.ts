69e4279a23d593c1eefefc8f003f5a6f1bf78722
Kevin Ashworth <kevinashworth@yahoo.com>
2025-02-13 09:53:35 -0800

    Chap 15
    But cookies bug

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,9 +1,28 @@
 "use server";
 
+import { AuthError } from "next-auth";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import postgres from "postgres";
 import { z } from "zod";
+
+import { signIn } from "@/actions/auth";
+
+export async function authenticate(prevState: string | undefined, formData: FormData) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
+}
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
